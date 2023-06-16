52a1aedb4adc51e74c3a535acc55fa246a56c997
DESKTOP-SMD51TC\kiik5 <kiik52@naver.com>
2023-06-16 21:48:31 +0900

    main 페이지 movie-info 추가

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -34,23 +34,98 @@ export default function MainComponent () {
             > 
               <SwiperSlide>
                 <img src="./img/main/sec01/sam-spiderman3.PNG" alt="" />
-                <Link to='/spiderman1'>
-                  <button className='movie-btn'>
-                    <span>Click to Movie</span>
-                  </button>
-                </Link>
+                <div className='slide-content'>
+                  <div className="movie-info">
+                    <h2>spider-man</h2>
+                    <p>
+                      Starring : Tobey Maguire
+                      <br/>
+                      Director : Sam Raimi
+                    </p>
+                    <p>After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy.</p>
+                    </div>
+                  <Link to='/spiderman1'>
+                    <button className='movie-btn'>
+                      <span>More-Movie</span>
+                    </button>
+                  </Link>
+                </div>
               </SwiperSlide>
               <SwiperSlide>
                 <img src="./img/main/sec01/amazing-spiderman-main.jpg" alt="" />
+                <div className='slide-content'>
+                  <div className="movie-info">
+                    <h2>The Amazing Spider-Man</h2>
+                    <p>
+                      Starring : Andrew Garfield
+                      <br/>
+                      Director : Marc Webb
+                    </p>
+                    <p>After Peter Parker is bitten by a genetically altered spider, he gains newfound, spider-like powers and ventures out to save the city from the machinations of a mysterious reptilian foe.</p>
+                  </div>
+                  <Link to='/spiderman2'>
+                  <button className='movie-btn'>
+                    <span>More-Movie</span>
+                  </button>
+                  </Link>
+                </div>
               </SwiperSlide>
               <SwiperSlide>
                 <img src="./img/main/sec01/tom-spicerman-photo.jpg" alt="" />
+                <div className='slide-content'>
+                  <div className="movie-info">
+                    <h2>Spider-Man: Homecoming</h2>
+                    <p>
+                      Starring : Tobey Maguire
+                      <br/>
+                      Director : Sam Raimi
+                    </p>
+                    <p>After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy.</p>
+                  </div>
+                  <Link to='/spiderman3'>
+                  <button className='movie-btn'>
+                    <span>More-Movie</span>
+                  </button>
+                  </Link>
+                </div>
               </SwiperSlide>
               <SwiperSlide>
                 <img src="./img/main/sec01/miles.jpg" alt="" />
+                <div className='slide-content'>
+                  <div className="movie-info">
+                    <h2>Spider-Man <br /> : Into the Spider-Verse</h2>
+                    <p>
+                      Starring : Jake Johnson (Miles Morales)
+                      <br/>
+                      Director : Sam Raimi
+                    </p>
+                    <p>Teen Miles Morales becomes the Spider-Man of his universe and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.</p>
+                  </div>
+                  <Link to='/spiderman4'>
+                  <button className='movie-btn'>
+                    <span>More-Movie</span>
+                  </button>
+                  </Link>
+                </div>
               </SwiperSlide>
               <SwiperSlide>
                 <img src="./img/main/sec01/venom.jpg" alt="" />
+                <div className='slide-content'>
+                  <div className="movie-info">
+                    <h2>The Venom</h2>
+                    <p>
+                      Starring : Tom Hardy
+                      <br/>
+                      Director : Ruben Fleischer
+                    </p>
+                    <p>A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. But the being takes a liking to Earth and decides to protect it.</p>
+                  </div>
+                  <Link to='/venom'>
+                  <button className='movie-btn'>
+                    <span>More-Movie</span>
+                  </button>
+                  </Link>
+                </div>
               </SwiperSlide>
             </Swiper>
             <button className="swiper-prev-btn">
