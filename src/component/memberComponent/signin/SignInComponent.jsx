4d6d5e63498df9b2e524339edf113881eea752eb
DESKTOP-SMD51TC\kiik5 <kiik52@naver.com>
2023-08-30 21:02:43 +0900

    Disign: 회원가입 구현 완료
    - SignInComponent (로그인) 페이지 구현
    - signin.scss 구현

--- src/component/memberComponent/signin/SignInComponent.jsx
+++ src/component/memberComponent/signin/SignInComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link}  from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default function SignInComponent () {
+export default function SignInComponent() {
   return (
     <main id="signInMain">
       <section id="signIn">
@@ -12,12 +12,12 @@ export default function SignInComponent () {
           <div className="content">
             <form id="signIn-form" name="sign_in" method="post" action="./response.php">
               <ul>
-                <li><input type="text" id="id" name="id" placeholder="아이디를 입력해주세요"/></li>
-                <li><input type="password" id="pw" name="pw" placeholder="비밀번호를 입력해주세요"/></li>
+                <li><input type="text" id="id" name="id" placeholder="아이디를 입력해주세요" /></li>
+                <li><input type="password" id="pw" name="pw" placeholder="비밀번호를 입력해주세요" /></li>
                 <li><a href="#!">아이디찾기</a><i>|</i><a href="#!">비밀번호찾기</a></li>
-                <li><button type="submit" className="submit-btn">로그인</button></li>
-                <li><Link to='/signup'><button type="button" className="member-signin-btn">회원가입</button></Link></li>
-              </ul>    
+                <li><button type="submit" className="submit-btn"><span>로그인</span></button></li>
+                <li><Link to='/signup'><button type="button" className="member-signin-btn"><span>회원가입</span></button></Link></li>
+              </ul>
             </form>
           </div>
         </div>
